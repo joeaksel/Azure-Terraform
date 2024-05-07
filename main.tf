1bf51169f596b5e3cf682ca82378a8414602517b
Joe Aksel <joe.aksel@gmail.com>
2024-05-07 19:15:31 -0400

    Updated main.tf, added Linux VM

--- main.tf
+++ main.tf
@@ -94,3 +94,30 @@ resource "azurerm_network_interface" "terraform-nic1" {
     environment = "terraform"
   }
 }
+
+resource "azurerm_linux_virtual_machine" "linuxvm1" {
+  name                  = "linuxvm1"
+  resource_group_name   = azurerm_resource_group.terraform-project.name
+  location              = azurerm_resource_group.terraform-project.location
+  size                  = "Standard_B1s"
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.terraform-nic1.id]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
